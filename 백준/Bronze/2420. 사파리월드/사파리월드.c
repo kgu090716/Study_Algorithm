10f5e79c901bbe2b56ac2a86e051070b22440d6f
KCPIT <kgu090716@gmail.com>
2023-04-22 23:05:36 +0900

    [Bronze V] Title: 사파리월드, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+int main(void)
+{
+    long long n, m;
+    scanf("%lld %lld", &n, &m);
+    if (n > m)
+        printf("%lld", n - m);
+    else
+        printf("%lld", m - n);
+    return 0;
+}
