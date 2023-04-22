af18b24c088f5c2860435d36a4502c75d6f621a2
KCPIT <kgu090716@gmail.com>
2023-04-22 23:10:03 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main(void)
+{
+	int N;
+	scanf("%d", &N);
+	for (int i = N; i > 0; i--)
+	{
+		for (int k = 0; k < N - i; k++)
+			printf(" ");
+		for (int j = i; j > 0; j--)
+		{
+
+			printf("*");
+		}
+		printf("\n");
+	}
+	return 0;
+}
