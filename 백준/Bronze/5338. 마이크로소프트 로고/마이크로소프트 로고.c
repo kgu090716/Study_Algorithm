26957ea81cf62d2378755f082462d9c52d48db8f
KCPIT <kgu090716@gmail.com>
2023-04-22 23:13:59 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("       _.-;;-._\n");
+    printf("'-..-'|   ||   |\n");
+    printf("'-..-'|_.-;;-._|\n");
+    printf("'-..-'|   ||   |\n");
+    printf("'-..-'|_.-''-._|\n");
+    return 0;
+}
