2b2deed02a3953b7e5813265efe14ff2bf94f474
KCPIT <kgu090716@gmail.com>
2023-04-22 23:09:58 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(void)
+{
+    int num, i, j;
+    scanf("%d", &num);
+    for (i = 0; i < num; i++)
+    {
+        for (j=0; j<=i; j++)
+            printf("*");
+        printf("\n");
+    }
+    return 0;
+}
