a3149ef1f3393a4b61f021e9bf501af37bc11f55
KCPIT <kgu090716@gmail.com>
2023-04-29 00:02:01 +0900

    [Silver II] Title: 스택 수열, Time: 1124 ms, Memory: 89004 KB -BaekjoonHub

---
+++ 백준/Silver/1874. 스택 수열/스택 수열.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+#include<string.h>
+int dat[10000005];
+int pos = 0;
+char res[10000005] = "";
+int arr[10000005];
+void push(int x)
+{
+    dat[pos++] = x;
+}
+
+void pop() {
+    pos--;
+}
+
+int top() {
+    return dat[pos - 1];
+}
+
+int main() {
+    int N;
+    int j = 0;
+    scanf("%d", &N);
+    for (int i = 0; i < N; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    for (int i = 1; i <= N; i++)
+    {
+        push(i);
+        strcat(res, "+\n");
+        while (dat[pos-1] == arr[j] && N>j)
+        {
+            pop();
+            strcat(res, "-\n");
+            j++;
+        }
+    }
+    if (N == j)
+        printf("%s", res);
+    else
+        printf("NO");
+    return 0;
+}
