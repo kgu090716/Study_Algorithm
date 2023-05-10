68e564c25da2a8e680b1d9b19955b92f12ab36c2
KCPIT <kgu090716@gmail.com>
2023-05-10 22:55:19 +0900

    [Silver I] Title: 그림, Time: 20 ms, Memory: 3252 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.cc
+++ 백준/Silver/1926. 그림/그림.cc
@@ -2,19 +2,23 @@
 using namespace std;
 #define X first
 #define Y second
+
 int board[502][502];
 bool vis[502][502];
 int n, m;
 int dx[4] = {1, 0, -1, 0};
 int dy[4] = {0, 1, 0, -1};
+
 int main(void)
 {
     ios::sync_with_stdio(0);
     cin.tie(0);
     int piccnt = 0;
-    int MaxArea = 0;
-    int cnt = 0;
+    int max_area = 0;
+    int area;
+    queue<pair<int, int>> Q;
     cin >> n >> m;
+
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < m; j++)
@@ -22,38 +26,49 @@ int main(void)
             cin >> board[i][j];
         }
     }
-    for(int i =0; i < n; i++)
+
+    for (int i = 0; i < n; i++)
     {
-        for(int j = 0; j < m; j++)
+        for (int j = 0; j < m; j++)
         {
-            if(board[i][j] && vis[i][j] != 1)
+            if (board[i][j] == 1 && vis[i][j] == 0)
             {
-                cnt = 0;
-                queue<pair<int, int>> Q;
+                area = 0;
                 vis[i][j] = 1;
-                Q.push({i,j});
-                while(!Q.empty())
+                Q.push({i, j});
+                piccnt++;
+                while (!Q.empty())
                 {
                     pair<int, int> cur = Q.front();
                     Q.pop();
-                    cnt++;
-                    for(int dir = 0; dir < 4; dir++)
+                    area++;
+                    for (int dir = 0; dir < 4; dir++)
                     {
                         int nx = cur.X + dx[dir];
                         int ny = cur.Y + dy[dir];
-                        if(nx < 0 || nx >=n || ny < 0 || ny >= m)
+                        if (nx < 0 || ny < 0 || nx >= n || ny >= m)
                             continue;
-                        if(vis[nx][ny] || board[nx][ny] != 1)
+                        if (vis[nx][ny] || board[nx][ny] != 1)
                             continue;
                         vis[nx][ny] = 1;
-                        Q.push({nx,ny});
+                        Q.push({nx, ny});
                     }
                 }
-                MaxArea = MaxArea > cnt ? MaxArea : cnt;
-                piccnt++;
+                max_area = area > max_area ? area : max_area;
             }
         }
     }
+
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = 0; j < m; j++)
+    //     {
+    //         cout << vis[i][j] << ' ';
+    //     }
+    //     cout << '\n';
+    // }
+
     cout << piccnt << '\n';
-    cout << MaxArea << '\n';
+    cout << max_area << '\n';
+    return 0;
 }
