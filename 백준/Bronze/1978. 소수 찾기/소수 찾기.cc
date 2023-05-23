408bec59ec88644617834d3224aa1a8e02da2d9f
KCPIT <kgu090716@gmail.com>
2023-05-24 01:49:58 +0900

    [Bronze II] Title: 소수 찾기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.cc
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(void)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int n;
+    int cnt = 0;
+    int input;
+    cin >> n;
+    for(int i = 0; i < n; i++)
+    {
+        int c = 0;
+        cin >> input;
+        for(int j = 1; j <= input; j++)
+        {
+            if(input % j == 0)
+                c++;
+            if(c >= 3)
+                break;
+        }
+        if(c == 2)
+            cnt++;
+    }
+    cout << cnt << '\n';
+}
