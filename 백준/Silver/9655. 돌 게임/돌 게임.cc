c7993ce807ea541f6e26036ea6f776488cde701f
KCPIT <kgu090716@gmail.com>
2023-07-02 02:32:27 +0900

    [Silver V] Title: 돌 게임, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/9655. 돌 게임/돌 게임.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int D[1002];
+int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int n;
+    cin >> n;
+    if(n%2==1)
+        cout << "SK" << '\n';
+    else
+        cout << "CY" << '\n';
+    return 0;
+}
